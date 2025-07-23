f26cf1cf25d06db44a0ec75f2f3990e1e320ca3c
mikigo <1964191531@qq.com>
2025-07-23 17:22:11 +0800

    update docs

--- scripts/json_to_md.py
+++ scripts/json_to_md.py
@@ -2,40 +2,22 @@ import os
 import json
 
 groups = [
-    "人教版小学",
-    "人教版初中",
-    "人教版高中",
+    "小学",
+    "初中",
+    "高中",
     "四级",
     "六级",
-    "考研",
-    "雅思",
-    "新东方",
-    "专四",
-    "专八",
 ]
-
-with open("./bookLists.txt", "r", encoding="utf-8") as f:
-    txt = json.load(f)
-
-books = txt.get("data").get("normalBooksInfo")
 
 
 for i in os.listdir("./books"):
-
-    title = None
-    for book in books:
-        if book.get("id") == os.path.splitext(i)[0]:
-            title = book.get("title")
-            break
-    if title is None:
-        raise ValueError
 
     with open(f"./books/{i}", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
     g = None
     for group in groups:
-        if title.startswith(group):
+        if i.startswith(group):
             g =  group
             break
     else:
@@ -46,8 +28,8 @@ for i in os.listdir("./books"):
     if not os.path.exists(f"../docs/english/{g}"):
         os.mkdir(f"../docs/english/{g}")
 
-    with open(f"../docs/english/{g}/{title}.md", "w", encoding="utf-8") as mdf:
-        mdf.write(f"# {title}\n\n")
+    with open(f"../docs/english/{g}/{os.path.splitext(i)[0]}.md", "w", encoding="utf-8") as mdf:
+        mdf.write(f"# {os.path.splitext(i)[0]}\n\n")
         for line in lines:
 
             line = line.strip()
@@ -62,41 +44,44 @@ for i in os.listdir("./books"):
 
             # 翻译
             trans: list = content.get("trans")
+            phone = content.get("phone")
             for t in trans:
-                mdf.write(f"**{t.get('descCn')}**\n\n")
-                mdf.write(f"{t.get('tranCn')}\n\n")
-                if t.get('descOther'):
-                    mdf.write(f"**{t.get('descOther')}**\n\n")
-                    mdf.write(f"{t.get('tranOther')}\n\n")
+                # mdf.write(f"**{t.get('descCn')}**\n\n")
+                mdf.write(f"{f'`{phone}` ' if phone else ''}**{t.get('tranCn')}**\n\n")
+                # if t.get('descOther'):
+                #     mdf.write(f"**{t.get('descOther')}**\n\n")
+                #     mdf.write(f"{t.get('tranOther')}\n\n")
 
             # 短语
             phrase: dict = content.get("phrase")
             if phrase:
-                mdf.write(f"**{phrase.get('desc')}**\n\n")
+                mdf.write(f":::tip{{title=🤩{phrase.get('desc')}}}\n\n")
                 for p in phrase.get('phrases'):
                     pCn = p.get('pCn')
                     pContent = p.get('pContent')
-                    mdf.write(f"{pContent} ({pCn})\n\n")
+                    mdf.write(f"- {pContent} ({pCn})\n\n")
+                mdf.write(f":::\n\n")
 
             # 例句
             sentence: dict = content.get("sentence")
             if sentence:
-                mdf.write(f"**{sentence.get('desc')}**\n\n")
+                mdf.write(f":::note{{title=🎤{sentence.get('desc')}}}\n\n")
                 for s in sentence.get('sentences'):
-                    mdf.write(f"{s.get('sContent')}\n\n")
-                    mdf.write(f"{s.get('sCn')}\n\n")
+                    mdf.write(f"- {s.get('sContent')} ({s.get('sCn')})\n\n")
+                mdf.write(f":::\n\n")
 
             # 同义词
             syno: dict = content.get("syno")
             if syno:
-                mdf.write(f"**{syno.get('desc')}**\n\n")
+                mdf.write(f":::warning{{title=🤔{syno.get('desc')}}}\n\n")
                 for s in syno.get('synos'):
                     hwds = s.get('hwds')
                     l = ""
                     for h in hwds:
                         l += f"{h.get('w')}"
                     l += f" ({s.get('tran')})"
-                    mdf.write(f"{l}\n\n")
+                    mdf.write(f"- {l}\n\n")
+                mdf.write(f":::\n\n")
 
 
 
