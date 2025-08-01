f7f4ca24021b17c0e79851ecac4653b013cbf346
mikigo <1964191531@qq.com>
2025-08-01 11:15:10 +0800

    update mikigo.site

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,45 +1,61 @@
-import { defineConfig } from 'rspress/config';
+import {defineConfig} from 'rspress/config';
+import sitemap from "rspress-plugin-sitemap";
 import path from "node:path";
 
 export default defineConfig({
-  globalStyles: path.join(__dirname, 'theme/var.css'),
-  root: 'docs',
-  base: '/',
-  title: 'mikigo.site',
-  description: 'mikigo.site',
-  icon: '/favicon.ico',
-  logo: '/logo.png',
-  logoText: 'mikigo.site',
-  themeConfig: {
-    enableContentAnimation: true,
-    enableAppearanceAnimation: true,
-    enableScrollToTop: true,
-    lastUpdated: true,
+    globalStyles: path.join(__dirname, 'theme/var.css'),
+    root: 'docs',
+    base: '/',
+    title: 'mikigo.site',
+    description: 'mikigo.site',
+    icon: '/favicon.ico',
+    logo: '/logo.png',
+    logoText: 'mikigo.site',
+    themeConfig: {
+        enableContentAnimation: true,
+        enableAppearanceAnimation: true,
+        enableScrollToTop: true,
+        lastUpdated: true,
 
-    footer: {
-      message: `
+        footer: {
+            message: `
         <a href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2024104386号</a>
          版权所有 © 2020-${new Date().getFullYear()} mikigo
       `,
-    },
-    hideNavbar: 'auto',
+        },
+        hideNavbar: 'auto',
 
-    outlineTitle: '本页目录',
-    prevPageText: '上一页',
-    nextPageText: '下一页',
-    lastUpdatedText: '最近更新时间',
-    searchPlaceholderText: '搜索文档',
-    overview: {
-      filterNameText: '快速查找',
-      filterPlaceholderText: '输入关键词',
-      filterNoResultText: '未查询到结果',
+        outlineTitle: '本页目录',
+        prevPageText: '上一页',
+        nextPageText: '下一页',
+        lastUpdatedText: '最近更新时间',
+        searchPlaceholderText: '搜索文档',
+        overview: {
+            filterNameText: '快速查找',
+            filterPlaceholderText: '输入关键词',
+            filterNoResultText: '未查询到结果',
+        },
+        socialLinks: [
+            {
+                icon: 'github',
+                mode: 'link',
+                content: 'https://github.com/mikigo/',
+            }
+        ],
     },
-    socialLinks: [
-      {
-        icon: 'github',
-        mode: 'link',
-        content: 'https://github.com/mikigo/',
-      }
+    plugins: [
+        sitemap({
+            domain: "https://mikigo.site",
+            customMaps: {
+                "/sample": {
+                    loc: "/sample",
+                    lastmod: "2025-08-27T07:44:43.422Z",
+                    priority: "0.7",
+                    changefreq: "always",
+                },
+            },
+            defaultChangeFreq: "monthly",
+            defaultPriority: "0.5",
+        }),
     ],
-  },
 });
