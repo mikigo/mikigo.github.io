1b909f329a00ab787722ab9e796a34ece25f09ac
mikigo <1964191531@qq.com>
2025-09-30 11:07:47 +0800

    update mikigo.site

--- rspress.config.ts
+++ rspress.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
     logoText: 'mikigo.site',
 
     head: [
-        ['script', { src: '/busuanzi.min.js', defer: '' }],
+        ['script', { src: '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js', defer: '' }],
     ],
 
     themeConfig: {
@@ -30,6 +30,7 @@ export default defineConfig({
             message: `
         <a href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2024104386号</a>
          版权所有 © 2020-${new Date().getFullYear()} <a href="https://github.com/mikigo" target="_blank">mikigo</a>
+          | <p> | 本站总访问量：<span id="busuanzi_site_pv" style={{ color: '#1cc088' }}>加载中...</span> 次</p>
       `,
         },
         hideNavbar: 'auto',
