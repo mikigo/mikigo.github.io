423395d65631bf51c1ccc436f8e14d42b6845ff6
mikigo <1964191531@qq.com>
2026-02-03 02:13:37 +0800

    修复了一些已知问题

---
+++ theme/index.tsx
@@ -0,0 +1,32 @@
+import {
+  Layout as BasicLayout,
+  getCustomMDXComponent as basicGetCustomMDXComponent,
+} from '@rspress/core/theme-original';
+
+
+const myStyle = {
+  fontSize: '2em',
+  fontWeight: 'bold',
+  center: true,
+}
+
+const Layout = () => (
+  <BasicLayout
+    beforeFeatures={
+          <div>
+            <h1 align="center"  style={myStyle}>
+              我创建的开源项目
+            </h1>
+          </div>
+        }
+    afterDocContent={
+            <div align="left" style={{fontSize: "0.8em", color: "gray"}}>
+                声明：本站所有文章，均为本站原创发布。任何个人或组织，在未征得本站同意时，禁止复制、盗用、采集、发布本站内容到任何网站、书籍等各类媒体平台。
+                <a href="https://github.com/mikigo/" style={{color: "#1cc088"}}>-- mikigo</a>
+            </div>
+        }
+  />
+);
+
+export { Layout };
+export * from '@rspress/core/theme-original';
