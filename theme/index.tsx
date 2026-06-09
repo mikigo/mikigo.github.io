6e48468935933d0088eed4859934c1d605f0f425
mikigo <1964191531@qq.com>
2026-06-09 10:12:27 +0800

    refactor: move page view counter from footer to article content area

--- theme/index.tsx
+++ theme/index.tsx
@@ -21,6 +21,10 @@ const Layout = () => (
         }
     afterDocContent={
             <div align="left" style={{fontSize: "0.8em", color: "gray"}}>
+                <span style={{display:"none"}} className="counter-container">
+                  阅读量 <span data-pv-page></span> 次
+                </span>
+                <br/>
                 声明：本站所有文章，均为本站原创发布。任何个人或组织，在未征得本站同意时，禁止复制、盗用、采集、发布本站内容到任何网站、书籍等各类媒体平台。
                 <a href="https://github.com/mikigo/" style={{color: "#1cc088"}}>-- mikigo</a>
             </div>
