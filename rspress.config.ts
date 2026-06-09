11f2faf26a16b67c457cd1e5f2e36cba5b1d5574
mikigo <1964191531@qq.com>
2026-06-09 09:37:20 +0800

    refactor: update counter labels to show site/today PV and UV

--- rspress.config.ts
+++ rspress.config.ts
@@ -29,9 +29,10 @@ export default defineConfig({
             message: `
         <script async src="/counter.js"></script>
         <span style="display:none" class="counter-container" data-counter-style="badge">
-          本站访问 <span data-pv-site></span> 次 ·
-          今日 <span data-pv-today></span> 次 ·
-          访客 <span data-uv-today></span> 人
+          本站访问量 <span data-pv-site></span> 次 ·
+          本站访客数 <span data-uv-site></span> 人 ·
+          今日访问量 <span data-pv-today></span> 次 ·
+          今日访客数 <span data-uv-today></span> 人
         </span>
         <br>
         <a style="text-decoration: underline" href="https://beian.miit.gov.cn/" target="_blank">蜀ICP备2024104386号</a>
